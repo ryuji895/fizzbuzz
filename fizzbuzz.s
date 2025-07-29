855a09c329db53f2f37ce97bc07592d94c004b42
ryuji895 <ryuji4263@gmail.com>
2025-07-29 13:12:21 +0900

    Update fizzbuzz.s

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -5,55 +5,44 @@ section .data
 section .text
 	global _start
 _start:
-	mov r12d,0x1	;r12-r15はsyscallで消えない
+	xor r12d,r12d
 	mov r13d,0x3
 	mov r14d,0x5
+	mov rdi,0x1	;標準出力
 mainLoop:
+	xor r15d,r15d	;flag
+	inc r12d
 	mov eax,r12d
 	xor edx,edx
 	div r13d
 	cmp edx,0
 	jne notfizz
-	mov r15d,0x1
-	call put_fizz
+	mov r15d,1
+	mov esi,fizz
+	mov edx,0x4
+	call put
 notfizz:
 	mov eax,r12d
 	xor edx,edx
 	div r14d
 	cmp edx,0
 	jne notbuzz
-	mov r15d,0x1
-	call put_buzz
+	mov r15d,1
+	mov esi,buzz
+	mov edx,0x4
+	call put
 notbuzz:
 	cmp r15d,0x1
-	jne notLf
-	xor r15d,r15d
-	call put_lf
-notLf:
-	inc r12d
-	cmp r12d,0x10
+	jne notlf
+	mov esi,lf
+	mov edx,0x1
+	call put
+notlf:
+	cmp r12d,0xf
 	jne mainLoop
 	mov eax,0x3c
 	syscall
-put_lf:
-	mov eax,0x1
-	mov edi,0x1
-	mov esi,lf
-	mov edx,0x1
-	syscall
-	ret
-put_fizz:
-	mov eax,0x1
-	mov edi,0x1
-	mov esi,fizz
-	mov edx,0x4
-	syscall
-	ret
-
-put_buzz:
-	mov eax,0x1
-	mov edi,0x1
-	mov esi,buzz
-	mov edx,0x4
+put:
+	mov rax,0x1
 	syscall
 	ret
